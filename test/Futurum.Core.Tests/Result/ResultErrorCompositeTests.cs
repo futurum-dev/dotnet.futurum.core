fad5fae70ace9425dd3571a7f5d4ce90ba7302ea
futurum-dev <me@futurum.dev>
2023-03-26 18:42:49 +0100

    Adding missing tests

--- test/Futurum.Core.Tests/Result/ResultErrorCompositeTests.cs
+++ test/Futurum.Core.Tests/Result/ResultErrorCompositeTests.cs
@@ -11,99 +11,181 @@ namespace Futurum.Core.Tests.Result;
 
 public class ResultErrorCompositeTests
 {
-    [Fact]
-    public void ToErrorStringSafe()
+    public class with_Parent
     {
-        var exceptionErrorMessage = Guid.NewGuid().ToString();
+        [Fact]
+        public void ToErrorStringSafe()
+        {
+            var exceptionErrorMessage = Guid.NewGuid().ToString();
 
-        var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
+            var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
 
-        var errorMessage = Guid.NewGuid().ToString();
+            var errorMessage = Guid.NewGuid().ToString();
 
-        var resultErrorMessage = errorMessage.ToResultError();
+            var resultErrorMessage = errorMessage.ToResultError();
 
-        var resultError = ResultErrorCompositeExtensions.ToResultError(resultErrorMessage, resultErrorException);
+            var resultError = ResultErrorCompositeExtensions.ToResultError(resultErrorMessage, resultErrorException);
 
-        var errorString = resultError.ToErrorStringSafe(",");
+            var errorString = resultError.ToErrorStringSafe(",");
 
-        errorString.Should().Be($"{errorMessage},{exceptionErrorMessage}");
-    }
+            errorString.Should().Be($"{errorMessage},{exceptionErrorMessage}");
+        }
 
-    [Fact]
-    public void ToErrorString()
-    {
-        var exceptionErrorMessage = Guid.NewGuid().ToString();
+        [Fact]
+        public void ToErrorString()
+        {
+            var exceptionErrorMessage = Guid.NewGuid().ToString();
 
-        var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
+            var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
 
-        var errorMessage = Guid.NewGuid().ToString();
+            var errorMessage = Guid.NewGuid().ToString();
 
-        var resultErrorMessage = errorMessage.ToResultError();
+            var resultErrorMessage = errorMessage.ToResultError();
 
-        var resultError = ResultErrorCompositeExtensions.ToResultError(resultErrorMessage, resultErrorException);
+            var resultError = ResultErrorCompositeExtensions.ToResultError(resultErrorMessage, resultErrorException);
 
-        var errorString = resultError.ToErrorString(",");
+            var errorString = resultError.ToErrorString(",");
 
-        var expectedExceptionErrorMessage = @$"System.Exception: {exceptionErrorMessage}
+            var expectedExceptionErrorMessage = @$"System.Exception: {exceptionErrorMessage}
    at Futurum.Core.Tests.Result.ResultErrorCompositeTests.ThrowException(String errorMessage) in";
-        errorString.Should().StartWith($"{errorMessage},{expectedExceptionErrorMessage}");
-        errorString.Should().EndWith(@$"test/Futurum.Core.Tests/Result/ResultErrorCompositeTests.cs:line 110");
-    }
+            errorString.Should().StartWith($"{errorMessage},{expectedExceptionErrorMessage}");
+            errorString.Should().EndWith(@$"test/Futurum.Core.Tests/Result/ResultErrorCompositeTests.cs:line 192");
+        }
 
-    [Fact]
-    public void ToErrorStructureSafe()
-    {
-        var exceptionErrorMessage = Guid.NewGuid().ToString();
+        [Fact]
+        public void ToErrorStructureSafe()
+        {
+            var exceptionErrorMessage = Guid.NewGuid().ToString();
 
-        var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
+            var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
 
-        var errorMessage = Guid.NewGuid().ToString();
+            var errorMessage = Guid.NewGuid().ToString();
 
-        var resultErrorMessage = errorMessage.ToResultError();
+            var resultErrorMessage = errorMessage.ToResultError();
 
-        var resultError = ResultErrorCompositeExtensions.ToResultError(resultErrorMessage, resultErrorException);
+            var resultError = ResultErrorCompositeExtensions.ToResultError(resultErrorMessage, resultErrorException);
 
-        var errorStructure = resultError.ToErrorStructureSafe();
+            var errorStructure = resultError.ToErrorStructureSafe();
 
-        var parentErrorMessage = errorStructure.Message;
-        parentErrorMessage.Should().Be(errorMessage);
-        
-        var childErrors = errorStructure.Children.ToList();
-        childErrors.Count.Should().Be(1);
+            var parentErrorMessage = errorStructure.Message;
+            parentErrorMessage.Should().Be(errorMessage);
 
-        childErrors[0].Message.Should().Be(exceptionErrorMessage);
-        childErrors[0].Children.Should().BeEmpty();
-    }
+            var childErrors = errorStructure.Children.ToList();
+            childErrors.Count.Should().Be(1);
 
-    [Fact]
-    public void ToErrorStructure()
-    {
-        var exceptionErrorMessage = Guid.NewGuid().ToString();
+            childErrors[0].Message.Should().Be(exceptionErrorMessage);
+            childErrors[0].Children.Should().BeEmpty();
+        }
 
-        var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
+        [Fact]
+        public void ToErrorStructure()
+        {
+            var exceptionErrorMessage = Guid.NewGuid().ToString();
 
-        var errorMessage = Guid.NewGuid().ToString();
+            var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
 
-        var resultErrorMessage = errorMessage.ToResultError();
+            var errorMessage = Guid.NewGuid().ToString();
 
-        var resultError = ResultErrorCompositeExtensions.ToResultError(resultErrorMessage, resultErrorException);
+            var resultErrorMessage = errorMessage.ToResultError();
 
-        var errorStructure = resultError.ToErrorStructure();
+            var resultError = ResultErrorCompositeExtensions.ToResultError(resultErrorMessage, resultErrorException);
 
-        var parentErrorMessage = errorStructure.Message;
-        parentErrorMessage.Should().Be(errorMessage);
-        
-        var childErrors = errorStructure.Children.ToList();
-        childErrors.Count.Should().Be(1);
-        
-        var expectedExceptionErrorMessage = @$"System.Exception: {exceptionErrorMessage}
+            var errorStructure = resultError.ToErrorStructure();
+
+            var parentErrorMessage = errorStructure.Message;
+            parentErrorMessage.Should().Be(errorMessage);
+
+            var childErrors = errorStructure.Children.ToList();
+            childErrors.Count.Should().Be(1);
+
+            var expectedExceptionErrorMessage = @$"System.Exception: {exceptionErrorMessage}
    at Futurum.Core.Tests.Result.ResultErrorCompositeTests.ThrowException(String errorMessage) in";
-        childErrors[0].Message.Should().StartWith(expectedExceptionErrorMessage);
-        childErrors[0].Message.Should().EndWith($@"test/Futurum.Core.Tests/Result/ResultErrorCompositeTests.cs:line 110");
-        childErrors[0].Children.Should().BeEmpty();
+            childErrors[0].Message.Should().StartWith(expectedExceptionErrorMessage);
+            childErrors[0].Message.Should().EndWith($@"test/Futurum.Core.Tests/Result/ResultErrorCompositeTests.cs:line 192");
+            childErrors[0].Children.Should().BeEmpty();
+        }
     }
 
-    private Exception ThrowException(string? errorMessage)
+    public class without_Parent
+    {
+        [Fact]
+        public void ToErrorStringSafe()
+        {
+            var exceptionErrorMessage = Guid.NewGuid().ToString();
+
+            var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
+
+            var resultError = ResultErrorCompositeExtensions.ToResultError(new []{resultErrorException});
+
+            var errorString = resultError.ToErrorStringSafe(",");
+
+            errorString.Should().Be(exceptionErrorMessage);
+        }
+
+        [Fact]
+        public void ToErrorString()
+        {
+            var exceptionErrorMessage = Guid.NewGuid().ToString();
+
+            var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
+
+            var resultError = ResultErrorCompositeExtensions.ToResultError(new []{resultErrorException});
+
+            var errorString = resultError.ToErrorString(",");
+
+            var expectedExceptionErrorMessage = @$"System.Exception: {exceptionErrorMessage}
+   at Futurum.Core.Tests.Result.ResultErrorCompositeTests.ThrowException(String errorMessage) in";
+            errorString.Should().StartWith(expectedExceptionErrorMessage);
+            errorString.Should().EndWith(@$"test/Futurum.Core.Tests/Result/ResultErrorCompositeTests.cs:line 192");
+        }
+
+        [Fact]
+        public void ToErrorStructureSafe()
+        {
+            var exceptionErrorMessage = Guid.NewGuid().ToString();
+
+            var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
+
+            var resultError = ResultErrorCompositeExtensions.ToResultError(new []{resultErrorException});
+
+            var errorStructure = resultError.ToErrorStructureSafe();
+
+            var parentErrorMessage = errorStructure.Message;
+            parentErrorMessage.Should().BeEmpty();
+
+            var childErrors = errorStructure.Children.ToList();
+            childErrors.Count.Should().Be(1);
+
+            childErrors[0].Message.Should().Be(exceptionErrorMessage);
+            childErrors[0].Children.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ToErrorStructure()
+        {
+            var exceptionErrorMessage = Guid.NewGuid().ToString();
+
+            var resultErrorException = ThrowException(exceptionErrorMessage).ToResultError();
+
+            var resultError = ResultErrorCompositeExtensions.ToResultError(new []{resultErrorException});
+
+            var errorStructure = resultError.ToErrorStructure();
+
+            var parentErrorMessage = errorStructure.Message;
+            parentErrorMessage.Should().BeEmpty();
+
+            var childErrors = errorStructure.Children.ToList();
+            childErrors.Count.Should().Be(1);
+
+            var expectedExceptionErrorMessage = @$"System.Exception: {exceptionErrorMessage}
+   at Futurum.Core.Tests.Result.ResultErrorCompositeTests.ThrowException(String errorMessage) in";
+            childErrors[0].Message.Should().StartWith(expectedExceptionErrorMessage);
+            childErrors[0].Message.Should().EndWith($@"test/Futurum.Core.Tests/Result/ResultErrorCompositeTests.cs:line 192");
+            childErrors[0].Children.Should().BeEmpty();
+        }
+    }
+
+    private static Exception ThrowException(string? errorMessage)
     {
         try
         {
